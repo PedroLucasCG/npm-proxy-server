c6a595fdb4e5debeaafb530ecfd7fb5dea64a2d4
PedroLucasCG <pedrowwww44@gmail.com>
2024-08-25 22:35:27 -0300

    tentativa de consertar

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const rateLimit = require('express-rate-limit')
+//const rateLimit = require('express-rate-limit')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
 //rate limiting
+/*
 const limiter = rateLimit({
     windowMS: 10 * 60 * 1000,
     max: 5
 })
+*/
 app.use(limiter)
 app.set('trust proxy', 1)
 
@@ -19,7 +21,7 @@ app.set('trust proxy', 1)
 app.use('/api', require('./routes'))
 
 //set static folder
-app.use(express.static('public'))
+//app.use(express.static('public'))
 
 app.use(cors({
     origin: "*",
